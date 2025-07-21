9564c834b7e7250eb8a5ad62a9230e1a7ce659ea
wklchris <wklchris@hotmail.com>
2025-07-21 00:18:03 -0700

    v0.1.0: Add version files.

---
+++ src/cubimg.hpp
@@ -0,0 +1,7 @@
+#ifndef CUBIMG_H
+#define CUBIMG_H
+
+#include "cube.hpp"
+#include "version.hpp"
+
+#endif  // CUBIMG_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
-#include "cube.hpp"
+#include "cubimg.hpp"
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
+        std::cout << cubimg::Version::version_app << '\n';
         std::cout << "Usage: cubimg algorithm" << '\n';
         std::cout << "Example: cubimg \"R U R' U'\"" << std::endl;
         return 1;
